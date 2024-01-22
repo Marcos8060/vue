4ba5c7d82bc5988a9695b2e3af3c6f9173bed297
MarcosDMT <marcosgav80@gmail.com>
2024-01-23 01:06:12 +0300

    initial commit

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      books:[
-        {title: 'Titaninc', url: 'Roselyda Ayoo'},
-        {title: 'My Lord and I', url: 'Marcos Ochieng'},
-        {title: 'Messages to young lovers', url: 'Nancy Van Pelt'},
-      ]
+      books: [
+        { title: "Titaninc", author: "Roselyda Ayoo", isFav: true },
+        { title: "My Lord and I", author: "Marcos Ochieng", isFav: false },
+        {
+          title: "Messages to young lovers",
+          author: "Nancy Van Pelt",
+          isFav: true,
+        },
+      ],
     };
   },
   methods: {
     toggleShowBooks() {
-      this.showBooks = !this.showBooks;
+      this.showBooks =!this.showBooks;
     },
+    toggleColor(book){
+      book.isFav =! book.isFav
+    }
   },
 });
 
